de91dcb750631268e1072d0071fc9029544a9043
Terry Kwon <terrykkwon@gmail.com>
2016-07-30 14:04:37 +0900

    Change name.

---
+++ app/src/main/java/com/unithon/openplaces/Authentication.java
@@ -0,0 +1,11 @@
+package com.unithon.openplaces;
+
+/**
+ * Created by Terry Kwon on 7/30/2016.
+ */
+public class Authentication {
+
+    public static final String CLIENT_ID = "Aom32SiimhswMgFUKiNr";
+    public static final String CLIENT_SECRET = "yynGYM7yhM";
+
+}
--- app/src/main/java/com/unithon/openplaces/NaverMapActivity.java
+++ app/src/main/java/com/unithon/openplaces/NaverMapActivity.java
@@ -3,7 +3,7 @@ package com.unithon.openplaces;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends AppCompatActivity {
+public class NaverMapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
