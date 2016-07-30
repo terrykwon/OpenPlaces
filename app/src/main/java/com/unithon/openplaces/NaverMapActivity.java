63f828706186e22bee2f3d1e9b3e83262a00127a
Noverish <embrapers263@gmail.com>
2016-07-30 15:28:43 +0900

    카테고리 버튼 추가, 검색 toolbar 추가

--- app/src/main/java/com/unithon/openplaces/NaverMapActivity.java
+++ app/src/main/java/com/unithon/openplaces/NaverMapActivity.java
@@ -1,7 +1,10 @@
 package com.unithon.openplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NaverMapActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class NaverMapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
+        Button searchButton = (Button) findViewById(R.id.search);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NaverMapActivity.this, SearchActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/unithon/openplaces/SearchActivity.java
+++ app/src/main/java/com/unithon/openplaces/SearchActivity.java
@@ -3,6 +3,7 @@ package com.unithon.openplaces;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Noverish on 2016-07-30.
@@ -11,6 +12,10 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
