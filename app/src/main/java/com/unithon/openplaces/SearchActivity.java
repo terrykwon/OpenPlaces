27845aa7351497a8f4c4dc654b96d0530c1158a4
Noverish <embrapers263@gmail.com>
2016-07-30 14:18:02 +0900

    Search Activity, layout 추가, manifest에 activity추가, gitignore에 misc.xml 추가

---
+++ app/src/main/java/com/unithon/openplaces/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.unithon.openplaces;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Noverish on 2016-07-30.
+ */
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
