9c4297809d468df49296e30dd8d89c1287d93330
Terry Kwon <terrykkwon@gmail.com>
2016-07-30 15:21:36 +0900

    Add FAB to map

---
+++ app/src/main/java/com/unithon/openplaces/SampleSearchActivity.java
@@ -0,0 +1,13 @@
+package com.unithon.openplaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_search);
+    }
+}
