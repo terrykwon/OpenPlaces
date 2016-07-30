33bb9ec7ea5b1c1d0f0eae4abc1c06342bcd4dc1
Noverish <embrapers263@gmail.com>
2016-07-30 15:53:48 +0900

    auto complete 기능 추가

--- app/src/main/java/com/unithon/openplaces/SearchActivity.java
+++ app/src/main/java/com/unithon/openplaces/SearchActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 /**
  * Created by Noverish on 2016-07-30.
@@ -17,5 +19,11 @@ public class SearchActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        AutoCompleteTextView searchText = (AutoCompleteTextView) findViewById(R.id.toolbar_search);
+        String[] countries = {"식당", "네일아트", "가나다", "식식식", "마포구청"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,countries);
+        searchText.setAdapter(adapter);
+        searchText.setThreshold(1);
     }
 }
